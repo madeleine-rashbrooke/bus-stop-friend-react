22a022ff605c66573b5dc97ffc62aa8414d79aa6
Madeleine <email": null>
2016-08-25 11:37:11 +1200

    Map rendered in MapDisplay component, still issues with css for display on page

--- components/mapdisplay.js
+++ components/mapdisplay.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 
-class MapDisplay extends React.Component {
+class MapDisplay extends Component {
   constructor (props) {
     super(props)
         // this.map = <canvas height=${window.innerHeight}, width=${window.innerWidth} id="canvas"></canvas>
+  }
+
+  componentDidMount() {
+    L.mapbox.accessToken = 'pk.eyJ1IjoibWFkZWxlaW5lLXJhc2hicm9va2UiLCJhIjoiY2lyeDVqdXRmMDA4OTJ5b2ZxZXc1dDg5dCJ9.6kyfgf7klxvu7UizZNzA9A';
+
+    var map = L.mapbox.map('map', 'mapbox.streets')
+      .setView([-41.2963, 174.7742], 15);
+
+    var featureLayer = L.mapbox.featureLayer()
+      .loadID("madeleine-rashbrooke.15b5e4nd")
+      .addTo(map)
   }
 
   render() {
@@ -16,6 +27,8 @@ class MapDisplay extends React.Component {
         <div id="mapWrapper">
             {/*this.map*/}
         </div>
+        <div id="map"></div>
+
         <p>One map layer should show this bus stop #{stop}</p>
         <p>One map layer should show this route #{route}</p>
       </div>
